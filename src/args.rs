4a20f91d1de1ccb6c9acb65be8c6e5b3b1a6b24a
Richard Dallaway <richard@dallaway.com>
2024-12-05 22:08:51 +0000

    add --fpr option

--- src/args.rs
+++ src/args.rs
@@ -23,11 +23,15 @@ pub enum Command {
         #[arg()]
         source: Option<PathBuf>,
 
-        /// The number of bits of storage for the filter:
+        /// Target false positive rate (0.0 to 1.0), if set used to computer the number of bits.
+        #[arg(long)]
+        fpr: Option<f64>,
+
+        /// The number of bits of storage for the filter (ignored if FPR is set).
         #[arg(long, default_value = "1024")]
         num_bits: usize,
 
-        /// How many items are expted to be stored.
+        /// How many items are expected to be stored.
         #[arg(long, default_value = "400000000")]
         num_items: usize,
     },
--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,16 @@ fn main() -> Result<()> {
             source,
             num_bits,
             num_items,
+            fpr,
         } => {
             args.filter_file.mk_parent_dir()?;
-            let mut filter = BloomFilter::with_num_bits(num_bits).expected_items(num_items);
+
+            let mut filter = if let Some(rate) = fpr {
+                BloomFilter::with_false_pos(rate).expected_items(num_items)
+            } else {
+                BloomFilter::with_num_bits(num_bits).expected_items(num_items)
+            };
+
             for line in source.open().lines() {
                 filter.insert(&line?);
             }
